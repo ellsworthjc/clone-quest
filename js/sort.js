c3e0d44870935a845898d81220308fa8a19c999e
Connor Ellsworth <ellsworthjc@gmail.com>
2023-09-19 20:57:20 -0400

    add sorting characters by trait

---
+++ js/sort.js
@@ -0,0 +1,21 @@
+/**
+ * Sorts character list by given trait in given order
+ * @param {Array} characters - array of characters
+ * @param {string} trait - only supports height or weight currently
+ * @param {string} order - "asc" or "desc" for ascending or descending order
+ * @returns {Array}
+ */
+export function sortCharactersByTrait(characters, trait="height", order="desc") {
+	if (trait === 'weight') {
+		trait = 'mass';
+	};
+
+	characters.sort((a, b) => {
+		if (order === 'asc') {
+			return Math.sign(parseInt(a[trait]) - parseInt(b[trait]));
+		}
+		return Math.sign(parseInt(b[trait]) - parseInt(a[trait]));
+	});
+
+	return characters;
+}

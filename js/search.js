ff8f946b32b8ebe5d5ab7dd4a3e29a222cc2782c
Connor Ellsworth <ellsworthjc@gmail.com>
2023-11-08 20:03:52 -0500

    update comments jsdoc

--- js/search.js
+++ js/search.js
@@ -1,12 +1,11 @@
 /**
  * Returns the 5 characters whose heights are closest to the user's
+ * Find 5 characters where abs(character.trait - userValue) is the smallest number
  * @param {string} trait - only supports height or mass currently
  * @param {number} userValue - a user's height/weight in cm/kg
  * @returns {Array} - 5 characters whose heights/weights are closest to the user's
  */
 export function topFiveMatchesByTrait(trait="height", userValue, data) {
-	// loop through data
-	// find 5 characters where abs(character.trait - userValue) is the smallest number
 	data.forEach(character => {
 		character['diff'] = Math.abs(character[trait] - userValue);
 	});
